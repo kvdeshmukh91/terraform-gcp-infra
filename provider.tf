033ba6cf2784080730830445df90f0aa06dc912e
Swaroo Desh <swaroodesh03@gmail.com>
2026-03-05 16:31:29 +0530

    trigger pipeline

--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  credentials = file("${path.module}/sa-key.json")
-  project = var.project_id
-  region  = var.region
-}
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "project_id" {}
-variable "region" {
-  default = "asia-south1"
-}
-variable "vm_name" {
-  default = "demo-vm"
-}
